acba841d5e1058051a03ed30ce351f23e618559f
harnoor-gandhi <56862515+harnoor-gandhi@users.noreply.github.com>
2023-08-07 18:32:56 +0530

    Add files via upload

---
+++ custom visualization/erratic-sand/visualizations/chemical-industry/linechart.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { LineChart } from 'nr1'
+
+export default class Linechart extends React.Component{
+    render(){
+        const linechart1 = {
+            metadata :{
+                id: 'line-chart-1',
+                name: 'Version A',
+                viz: 'main',
+                color: 'red'
+            },
+            data :[
+                {x: 0 , y:0},
+                {x: 10, y:20},
+                {x: 30, y:40}
+            ]
+        }
+        //  return <LineChart data={[linechart1]} fullWidth />;
+        return <LineChart
+        accountId={4038254}
+        query="SELECT average(host.net.transmitBytesPerSecond) AS 'Transmit bytes per second', average(host.net.receiveBytesPerSecond) AS 'Receive bytes per second' FROM Metric WHERE `entityGuid` = 'NDAzODI1NHxJTkZSQXxOQXwyOTQzNzg5OTI0NTg1NzQ0MjQx' TIMESERIES auto" 
+        fullWidth
+      />;
+    }
+}
